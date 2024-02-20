0cec24fb65d450a241cb1ce9c81d55c89c0221b2
Andrea Colombo <acolombodev@gmail.com>
2024-02-20 12:07:09 +0100

    Add custom fonts

--- app/src/main/java/com/willbsp/habits/ui/theme/Type.kt
+++ app/src/main/java/com/willbsp/habits/ui/theme/Type.kt
@@ -1,34 +1,73 @@
 package com.willbsp.habits.ui.theme
 
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.willbsp.habits.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val Typography
+    @Composable
+    get() = MaterialTheme.typography.copy(
+        displayLarge = MaterialTheme.typography.displayLarge.copy(
+            fontFamily = Fonts.Unbounded
+        ),
+        displayMedium = MaterialTheme.typography.displayMedium.copy(
+            fontFamily = Fonts.Unbounded
+        ),
+        displaySmall = MaterialTheme.typography.displaySmall.copy(
+            fontFamily = Fonts.Unbounded
+        ),
+        headlineLarge = MaterialTheme.typography.headlineLarge.copy(
+            fontFamily = Fonts.Unbounded
+        ),
+        headlineMedium = MaterialTheme.typography.headlineMedium.copy(
+            fontFamily = Fonts.Unbounded
+        ),
+        headlineSmall = MaterialTheme.typography.headlineSmall.copy(
+            fontFamily = Fonts.Unbounded
+        ),
+        titleLarge = MaterialTheme.typography.titleLarge.copy(
+            fontFamily = Fonts.Unbounded,
+        ),
+        titleMedium = MaterialTheme.typography.titleMedium.copy(
+            fontFamily = Fonts.Unbounded,
+        ),
+        titleSmall = MaterialTheme.typography.titleSmall.copy(
+            fontFamily = Fonts.Unbounded,
+        ),
+        bodyLarge = MaterialTheme.typography.bodyLarge.copy(
+            fontFamily = Fonts.WorkSans,
+        ),
+        bodyMedium = MaterialTheme.typography.bodyMedium.copy(
+            fontFamily = Fonts.WorkSans,
+        ),
+        bodySmall = MaterialTheme.typography.bodySmall.copy(
+            fontFamily = Fonts.WorkSans,
+        ),
+        labelLarge = MaterialTheme.typography.labelLarge.copy(
+            fontFamily = Fonts.Unbounded,
+        ),
+        labelMedium = MaterialTheme.typography.labelMedium.copy(
+            fontFamily = Fonts.Unbounded,
+        ),
+        labelSmall = MaterialTheme.typography.labelSmall.copy(
+            fontFamily = Fonts.Unbounded,
+        ),
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+object Fonts {
+    val Unbounded = FontFamily(
+        Font(R.font.unbounded_regular, FontWeight.Normal),
+        Font(R.font.unbounded_medium, FontWeight.Medium),
+        Font(R.font.unbounded_semi_bold, FontWeight.SemiBold),
     )
-    */
-)
+    val WorkSans = FontFamily(
+        Font(R.font.work_sans_regular, FontWeight.Normal),
+        Font(R.font.work_sans_semi_bold, FontWeight.SemiBold),
+    )
+}
+
+
+
